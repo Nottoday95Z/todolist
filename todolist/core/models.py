30c81024f6b3934b7af77f64c547b20509e1d19f
nottoday95z <nottoday95z@gmail.com>
2022-12-20 19:10:04 +0300

    Initial commit

---
+++ todolist/core/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50, verbose_name="Имя")
+    last_name = models.CharField(max_length=50, verbose_name="Фамилия")
+    username = models.CharField(max_length=50, unique=True, verbose_name="Имя пользователя")
+    email = models.EmailField(max_length=100, unique=False, null=True, verbose_name="e-mail")
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
